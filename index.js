19986f779e71a5442e0aea41e931c00b55b21d19
Foam_Ranther <yashzzz321@gmail.com>
2024-09-16 16:06:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    document.querySelector('form').addEventListener('submit',(event) => {
+        event.preventDefault();
+        let input=document.querySelector('#currency');
+        let amount=document.querySelector('#amount');
+        let response=fetch("https://api.exchangeratesapi.io/latest?base=USD&access_key=c6674def7cb892bb749070a773461e76").then((response)=>{
+
+            return response.json();
+        }).then((data)=>{
+            const rate=data.rates[input.value.toUpperCase()];
+            if (rate===undefined){
+                document.querySelector('#rates').innerHTML=`Invalid Currency`;
+                
+            }
+            else{
+                let final_value=rate*amount.value
+                document.querySelector('#rates').innerHTML=`1 USD is equal to ${final_value}`;
+            }
+            input.value="";
+            amount.value="";
+            
+        }).catch((error)=>{
+            alert(`error ${error}`);
+            
+        })
+        
+        
+    }
+    )
+});
